98402fbf4e7107d123d0bd87ce84a8402c577c11
Ck Web Gaming Studios <ckwebgaming@gmail.com>
2026-06-02 17:56:53 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Topbar from "./components/Topbar";
+import BrowserPage from "./pages/BrowserPage";
+import ProfilePage from "./pages/ProfilePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import AuthErrorPage from "./pages/AuthErrorPage";
+import AuthPage from "./pages/AuthPage";
+
+export default function App() {
+  return (
+    <div className="layout">
+      <Sidebar />
+      <div className="page">
+        <Topbar />
+        <div className="page-content">
+          <Routes>
+            <Route path="/" element={<Navigate to="/clients" replace />} />
+            <Route path="/clients" element={<BrowserPage type="client" />} />
+            <Route path="/mods"    element={<BrowserPage type="mod" />} />
+            <Route path="/skins"   element={<BrowserPage type="skin" />} />
+            <Route path="/profile/:uid" element={<ProfilePage />} />
+            <Route path="/login"        element={<AuthPage />} />
+            <Route path="/auth/callback" element={<AuthCallbackPage />} />
+            <Route path="/auth/error"    element={<AuthErrorPage />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
