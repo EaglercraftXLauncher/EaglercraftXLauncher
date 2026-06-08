928092e2ddc1e5d6e1594294e7f44b992c00e34b
Ck Web Gaming Studios <ckwebgaming@gmail.com>
2026-06-08 17:58:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+      plugins: [
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler", { target: "18" }]]
+        }
+      }),
+      Sitemap({
+        hostname: "https://eaglercraft2ck.pages.dev",
+        generateRobotsTxt: false
+      })
+  ],
   server: {
     port: 5173,
     proxy: {
