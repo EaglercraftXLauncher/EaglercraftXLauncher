86a7778e7f1566d3a05482f0f1d3e8dcfeed54dc
Ck Web Gaming Studios <ckwebgaming@gmail.com>
2026-06-08 18:30:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import Sitemap from "vite-plugin-sitemap"; // Added this missing import
 
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler", { target: "18" }]]
-      }
-    }),
+    react(),
     Sitemap({
       hostname: "https://eaglercraft2ck.pages.dev",
       generateRobotsTxt: false
